ebc1b5a7b37068202a82ba295bc6b0b5b7403282
eyijean8-dev <eyijean8@gmail.com>
2025-12-20 14:18:00 +0100

    Ajout Google Analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -110,6 +110,15 @@
     
     {{-- ANALYTICS --}}
     <!-- Google Analytics / Matomo -->
+     <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q7RTMZ39BD"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-Q7RTMZ39BD');
+    </script>
     
 </head>
 
