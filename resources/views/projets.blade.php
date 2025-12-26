4c5e4b12fbbc530c10b264cb658e982aed2e43f7
eyijean8-dev <eyijean8@gmail.com>
2025-12-26 13:30:10 +0100

    Switch mailer to Resend (Render compatible)

--- resources/views/projets.blade.php
+++ resources/views/projets.blade.php
@@ -282,7 +282,7 @@
                 <div class="bg-white rounded-2xl shadow-xl overflow-hidden border border-gray-100 hover:shadow-2xl transition-all duration-500 hover:-translate-y-2">
                     <!-- Image avec overlay -->
                     <div class="relative overflow-hidden">
-                        <img src="{{ asset('images/gas-location.png') }}" 
+                        <img src="{{ asset('images/location.png') }}" 
                             alt="Emplacement bouteilles de gaz" 
                             class="w-full h-64 object-cover group-hover:scale-110 transition-transform duration-700">
                         <div class="absolute inset-0 bg-gradient-to-t from-black/50 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500"></div>
